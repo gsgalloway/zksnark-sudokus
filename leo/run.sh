35d81d482c9a2960f12b1be159b94e4a82cb35a0
Gavi Galloway <gsgalloway@users.noreply.github.com>
2022-10-26 11:12:12 -0600

    leo hello-world

---
+++ leo/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+BASEDIR=$(dirname $(readlink -f "$0"))
+IMAGE=gavisc/aleo-leo:latest
+
+docker run --rm \
+    -v $BASEDIR/sudoku:/sudoku \
+    -w /sudoku \
+    $IMAGE \
+    leo build
