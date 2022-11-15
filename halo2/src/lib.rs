a72d916e14787f6ebeaebe3bcade2e690c85389b
Gavi Galloway <gsgalloway@users.noreply.github.com>
2022-11-15 15:26:41 -0500

    halo2 wip (can load puzzle into public_inputs)

--- halo2/src/lib.rs
+++ halo2/src/lib.rs
@@ -1,3 +1,4 @@
+use halo2::plonk::{Column, Instance};
 use halo2wrong::{
     halo2::{
         arithmetic::FieldExt,
@@ -14,12 +15,18 @@ use std::marker::PhantomData;
 #[derive(Clone, Debug)]
 pub struct SudokuConfig {
     main_gate_config: MainGateConfig,
+    board: Column<Instance>,
 }
 
 impl SudokuConfig {
     pub fn new<F: FieldExt>(meta: &mut ConstraintSystem<F>) -> Self {
         let main_gate_config = MainGate::configure(meta);
-        SudokuConfig { main_gate_config }
+        let board = meta.instance_column();
+        meta.enable_equality(board);
+        SudokuConfig {
+            main_gate_config,
+            board,
+        }
     }
 
     fn main_gate<F: FieldExt>(&self) -> MainGate<F> {
@@ -29,7 +36,7 @@ impl SudokuConfig {
 
 #[derive(Clone, Debug, Default)]
 pub struct SudokuCircuit<F: FieldExt> {
-    pub board: Array2<u8>,
+    pub puzzle: Array2<u8>,
     pub solution: Array2<u8>,
     marker: PhantomData<F>,
 }
@@ -52,14 +59,20 @@ impl<F: FieldExt> Circuit<F> for SudokuCircuit<F> {
         config: Self::Config,
         mut layouter: impl Layouter<F>,
     ) -> Result<(), Error> {
+        let main_gate: MainGate<F> = config.main_gate();
+        let mut board_cells: Array2<AssignedCell<F, F>> = array![[]];
+
         layouter.assign_region(
             || "region 0",
             |region| {
                 let offset = 0;
                 let ctx = &mut RegionCtx::new(region, offset);
 
-                // load the given solution (private) into the circuit
-                let solution_cells = self.load_solution(&config, ctx)?;
+                // load the board (public) into the circuit
+                board_cells = self.load_board(&config, ctx, &self.puzzle)?;
+
+                // load the solution (private) into the circuit
+                let solution_cells = self.load_board(&config, ctx, &self.solution)?;
 
                 // check that both the rows and the columns are both valid
                 // using ndarray here to abstract away the index math for slicing rows and columns
@@ -81,30 +94,68 @@ impl<F: FieldExt> Circuit<F> for SudokuCircuit<F> {
                     }
                 }
 
-                // load in the board (public) and check that the solution matches
+                // check that the solution matches the board
+                // check each cell in `board` is either zero or equals the corresponding cell in `solution`
+                for row_idx in 0..9 {
+                    for col_idx in 0..9 {
+                        let board_cell = &board_cells[[row_idx, col_idx]];
+                        let solution_cell = &solution_cells[[row_idx, col_idx]];
+                        let board_cell_is_zero = main_gate.is_zero(ctx, &board_cell)?;
+                        let board_cell_equals_solution =
+                            main_gate.is_equal(ctx, board_cell, solution_cell)?;
+                        main_gate.one_or_one(
+                            ctx,
+                            &board_cell_is_zero,
+                            &board_cell_equals_solution,
+                        )?;
+                    }
+                }
+
+                // expose the board as public inputs
+                // for (i, cell) in board_cells.iter().enumerate() {
+                // region.constrain_instance(board_cells[[0, 0]].cell(), config.board, 0)?;
+                // }
 
                 Ok(())
             },
         )?;
+
+        // layouter.constrain_instance(board_cells[[0, 0]].cell(), config.board, 0)?;
+        // layouter.constrain_instance(board_cells[[0, 1]].cell(), config.board, 1)?;
+
+        // main_gate.expose_public(layouter, cell, public_input_idx)?;
+
+        // mark each cell of the board as public input
+        for (public_input_idx, assigned_value) in board_cells.iter().enumerate() {
+            layouter.constrain_instance(assigned_value.cell(), config.board, public_input_idx)?;
+        }
+
+        // .map(|(public_input_idx, board_cell)| {
+        //     main_gate.expose_public(layouter, board_cell, public_input_idx)
+        // });
+        // main_gate.expose_public(layouter, board_cells[[0, 0]].clone(), 0)?;
+        // main_gate.expose_public(layouter, board_cells[[0, 1]].clone(), 1)?;
+        // layouter.constrain_instance(board_cells[[0, 0]], main_gate.loaded()., row)
 
         Ok(())
     }
 }
 
 impl<F: FieldExt> SudokuCircuit<F> {
-    fn load_solution(
+    fn load_board(
         &self,
         config: &SudokuConfig,
         ctx: &mut RegionCtx<F>,
+        board: &Array2<u8>,
     ) -> Result<Array2<AssignedCell<F, F>>, Error> {
         let main_gate: MainGate<F> = config.main_gate();
 
-        let solution_cells: Array2<AssignedCell<F, F>> = self.solution.mapv(|value| {
+        let loaded_cells: Array2<AssignedCell<F, F>> = board.mapv(|value| {
             let value = Value::known(F::from_u128(u128::from(value)));
             main_gate.assign_value(ctx, value).unwrap()
         });
 
-        Ok(solution_cells)
+        Ok(loaded_cells)
     }
 
     fn check_nine_cells<'a, I>(
@@ -133,6 +184,24 @@ impl<F: FieldExt> SudokuCircuit<F> {
 
         Ok(())
     }
+
+    // fn check_solution_against_board(
+    //     &self,
+    //     config: &SudokuConfig,
+    //     ctx: &mut RegionCtx<F>,
+    //     layouter: impl Layouter<F>,
+    //     solution_cells: &Array2<AssignedCell<F, F>>,
+    // ) -> Result<(), Error> {
+    //     let main_gate: MainGate<F> = config.main_gate();
+
+    //     // load board
+    //     let board_cells: Array2<AssignedCell<F, F>> = self.board.mapv(|value| {
+    //         let value = Value::known(F::from_u128(u128::from(value)));
+    //         main_gate.assign_value(ctx, value).unwrap()
+    //     });
+
+    //     Ok(())
+    // }
 }
 #[cfg(test)]
 mod test {
@@ -143,11 +212,11 @@ mod test {
         use halo2wrong::halo2::{dev::MockProver, halo2curves::bn256::Fr as Fp};
 
         // The number of rows in our circuit cannot exceed 2^k.
-        let k = 10;
+        let k = 11;
 
         // Instantiate the circuit with its inputs
         let circuit = SudokuCircuit {
-            board: array![
+            puzzle: array![
                 [0, 0, 0, 2, 6, 0, 7, 0, 1],
                 [6, 8, 0, 0, 7, 0, 0, 9, 0],
                 [1, 9, 0, 0, 0, 4, 5, 0, 0],
@@ -192,13 +261,17 @@ mod test {
             .render(5, &circuit, &drawing_area)
             .unwrap();
 
-        // Arrange the public input. We expose the multiplication result in row 0
-        // of the instance column, so we position it there in our public inputs.
-        let mut public_inputs: Vec<Fp> = circuit
-            .board
+        // Arrange the public inputs.
+        // `maingate` registers its own Instance column which we are not using, so that column's
+        // inputs can be skipped.
+        // The puzzle is loaded into our own column
+        let public_input_maingate = vec![];
+        let public_input_puzzle: Vec<Fp> = circuit
+            .puzzle
             .mapv(|value| Fp::from_u128(u128::from(value)))
             .into_raw_vec();
-        let mut public_inputs = vec![public_inputs];
+
+        let public_inputs = vec![public_input_maingate, public_input_puzzle];
 
         // Given the correct public input, our circuit will verify.
         let prover = MockProver::run(k, &circuit, public_inputs).unwrap();
